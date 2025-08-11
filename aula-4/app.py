df6897c6175bad497f393ac3fc4834280f3c84e1
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2025-08-11 12:41:42 -0300

    update app.py

--- aula-4/app.py
+++ aula-4/app.py
@@ -12,3 +12,22 @@ st.set_page_config(
 
 # --- Carregamento dos dados ---
 df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+
+# --- Barra Lateral (Filtros) ---
+st.sidebar.header("🔎 Filtros")
+
+# Filtro de Ano
+anos_disponiveis = sorted(df['ano'].unique())
+anos_selecionados = st.sidebar.multiselect("Ano", anos_disponiveis, default=anos_disponiveis)
+
+# Filtro de Senioridade
+senioridades_disponiveis = sorted(df['senioridade'].unique())
+senioridades_selecionadas = st.sidebar.multiselect("Senioridade", senioridades_disponiveis, default=senioridades_disponiveis)
+
+# Filtro por Tipo de Contrato
+contratos_disponiveis = sorted(df['contrato'].unique())
+contratos_selecionadas = st.sidebar.multiselect("Tipo de contrato", contratos_disponiveis, default=contratos_disponiveis)
+
+# Filtro por Tamanho da Empresa
+tamanhos_disponiveis = sorted(df['tamanho_empresa'].unique())
+tamanhos_selecionadas = st.sidebar.multiselect("Tamanhos de empresa", tamanhos_disponiveis, default=tamanhos_disponiveis)
